6d173c7f3b21577441222a58ba81aba5d4afea7d
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-10-10 13:55:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#----Numerical Analysis Dersi için Python genel tekrar Notları----
+
+#Değişkenler ve Veri Türleri:
+
+x = 5  # Bir tamsayı (int) değişkeni tanımlama
+y = 3.14  # Bir ondalık sayı (float) değişkeni tanımlama
+ad = "Bedirhan"  # Bir metin (string) değişkeni tanımlama
+
+#Koşullu İfadeler:
+if x > 0:
+    print("x pozitif")
+elif x < 0:
+    print("x negatif")
+else:
+    print("x sıfır")
