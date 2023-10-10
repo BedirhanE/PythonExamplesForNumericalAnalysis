235ebd14d14c1886a28366f27e3fe10c0a0282e5
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-10-10 14:16:41 +0300

    Pythonda temel işlemleri kullandığım bir örnek uygulama ekledim.

--- main.py
+++ main.py
@@ -13,3 +13,25 @@ elif x < 0:
     print("x negatif")
 else:
     print("x sıfır")
+
+#Döngüler
+for i in range(5):
+    print(i)  # 0'dan 4'e kadar sayıları yazdırır.
+
+while x > 0:
+    print(x)
+    x -= 1
+
+#Liste
+my_list = [1, 2, 3, 4, 5]
+
+#Fonksiyon
+def toplama(a, b):
+    return a + b
+
+sonuc = toplama(3, 4)  # 3 ile 4'ü toplar ve sonucu döndürür
+
+#Modül
+import math
+x = math.sqrt(25)  # 25'in karekökünü hesaplar
+
