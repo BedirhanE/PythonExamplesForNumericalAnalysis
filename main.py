302a4733ea1b63886b32a612f1625b78d38df7eb
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-10-17 23:50:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 x = 5  # Bir tamsayı (int) değişkeni tanımlama
 y = 3.14  # Bir ondalık sayı (float) değişkeni tanımlama
-ad = "Bedirhan"  # Bir metin (string) değişkeni tanımlama
+ad = "Bedirhan"  # Bir metin (string) değişkeni tanımlama.
 
 #Koşullu İfadeler:
 if x > 0:
