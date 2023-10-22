df57cf33e936e358a32c4acf49eb47ce89108cce
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-10-23 02:39:35 +0300

    Update Project

---
+++ module_using.py
@@ -0,0 +1,21 @@
+import HesapModulu
+
+HesapModulu.yeni_maas(1000)
+
+HesapModulu.yeni_maas(6000)
+
+import HesapModulu as hm
+
+hm.yeni_maas(2540)
+hm.yeni_maas(4850)
+
+
+from HesapModulu  import yeni_maas
+yeni_maas(1254)
+yeni_maas(7840)
+
+#3 farklı mödül kullanarak fonk. çağırma metodu
+
+
+import HesapModulu as  hm
+print("Maaşlar:",hm.maaslar)
